522e66c387ced78bf3efe35cfecc9a12c989f724
Christoffer Jansson <ctjansson@protonmail.com>
2021-12-01 16:06:33 +0100

    Converted string array to int array, started part b

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] lines = File.ReadAllLines("./input.txt");
+List<string> lines = File.ReadAllLines("./input.txt").ToList();
+List<int> numbers = new List<int>();
 
-int increament = 0;
+foreach (var line in lines)
+{
+    numbers.Add(int.Parse(line));
+}
 
-for (int i = 1; i < lines.Length; i++)
+int increamentPartA = 0;
+int increamentPartB = 0;
+
+for (int i = 1; i < lines.Count; i++)
 {
     int.TryParse(lines[i-1], out int lineA);
     int.TryParse(lines[i], out int lineB);
 
     if (lineA < lineB) 
     {
-        increament++;
+        increamentPartA++;
     }
+
+    
 }
 
+Console.WriteLine(increamentPartA.ToString());
 Console.ReadLine();
