616283115f22323a359091fc722c645e6a8000c3
Christoffer Jansson <ctjansson@protonmail.com>
2021-12-01 15:55:36 +0100

    Advent of Code 2021 - 1 December Part 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] lines = File.ReadAllLines("./input.txt");
+
+int increament = 0;
+
+for (int i = 1; i < lines.Length; i++)
+{
+    int.TryParse(lines[i-1], out int lineA);
+    int.TryParse(lines[i], out int lineB);
+
+    if (lineA < lineB) 
+    {
+        increament++;
+    }
+}
+
+Console.ReadLine();
